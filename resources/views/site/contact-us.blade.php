caf952b822e5045db09e45ad10ee528631394cda
Asmaa Email <asmaaesmail0@gmail.com>
2020-08-31 17:10:25 +0200

    front

--- resources/views/site/contact-us.blade.php
+++ resources/views/site/contact-us.blade.php
@@ -16,12 +16,13 @@
 </section>
 <!-- ///////||||||||||||||||||end breadCrumbs|||||||||||||||||||||||||||//// -->
 
+<div id="map"></div>
 
 <section class="address">
     <div class="container">
         <div class="row">
             <div class="col-md-4">
-                <div class="haveadrs">
+                <div class="haveadrs animate__animated wow animate__slideInUp">
                     <img src="{{asset('website/dist/img/phone.png')}}" alt="">
                     <div class="contntbb">
                         <b>العنوان</b>
@@ -29,15 +30,15 @@
                     </div>
                     <div class="contntbb">
                         <b>رقم الهاتف</b>
-                        <a  href="tel:+999485489522">+999485489522</b>
+                        <a  href="tel:+999485489522">+999485489522</b></a>
                     </div>
                     <div class="contntbb">
                         <b>الجوال </b>
-                        <a  href="tel:+999485489522">+999485489522</b>
+                        <a  href="tel:+999485489522">+999485489522</b></a>
                     </div>
                     <div class="contntbb">
                         <b>البريد الالكتروني </b>
-                        <a  href="mailto:webmaster@example.com">hawafel@admin.com</b>
+                        <a  href="mailto:webmaster@example.com">hawafel@admin.com</b></a>
                     </div>
                 </div>
             </div>
@@ -53,18 +54,27 @@
     </p>
     <div class="formContact ">
             <form class="">
-                <div class="form-group">
-                <input type="text" class="form-control inputspn" placeholder="اسم المستخدم">
-                <span class="focus-bg"></span>
+                <div class="row">
+                <div class="col-xs-12">
+                    <div class="form-group">
+                    <input type="text" class="form-control inputspn" placeholder="الاسم">
+                    </div>
                 </div>
-                <div class="form-group">
-                <input type="email" class="form-control inputspn" placeholder="رقم الجوال">
-                <span class="focus-bg"></span>
+                <div class="col-md-6 col-xs-12">
+                    <div class="form-group">
+                    <input type="email" class="form-control inputspn" placeholder="البريد الالكتروني">
+                    </div>
                 </div>
-
-                <div class="form-group">
-                <textarea rows="3" type="text" class="form-control inputspn" placeholder="الرسالة"></textarea>
-                <span class="focus-bg"></span>
+                <div class="col-md-6 col-xs-12">
+                    <div class="form-group">
+                    <input type="number" class="form-control inputspn" placeholder="رقم الجوال">
+                    </div>
+                </div>
+                <div class="col-xs-12">
+                    <div class="form-group">
+                    <textarea rows="3" type="text" class="form-control inputspn" placeholder="الرسالة"></textarea>
+                    </div>
+                </div>
                 </div>
                <!--  <button href="#" class="btn-hvr cartmargtop a-btn btnauto " type="submit">
                     ارسال
@@ -102,4 +112,46 @@
 @endsection
 @section('scripts')
 <script src="https://tympanus.net/Development/TextInputEffects/js/classie.js"></script>
+
+<script>
+         // The following example creates a marker in Stockholm, Sweden using a DROP
+         // animation. Clicking on the marker will toggle the animation between a BOUNCE
+         // animation and no animation.
+
+
+
+         function initMap() {
+             var map = new google.maps.Map(document.getElementById('map'), {
+                 zoom: 8,
+                 center: {
+                     lat: 26.3515732,
+                     lng: 45.3483186
+                 }
+             });
+         var marker;
+             marker = new google.maps.Marker({
+                 map: map,
+                 draggable: true,
+                 animation: google.maps.Animation.DROP,
+                 position: {
+                     lat: 26.3515732,
+                     lng: 45.3483186
+                 }
+             });
+             marker.addListener('click', toggleBounce);
+         }
+
+         function toggleBounce() {
+             if (marker.getAnimation() !== null) {
+                 marker.setAnimation(null);
+             } else {
+                 marker.setAnimation(google.maps.Animation.BOUNCE);
+             }
+         }
+
+      </script>
+
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAiOgBoR0bz-6mj23O0Wmk1CScy41joFMY&callback=initMap"
+    async defer></script>
+
 @endsection
