3febcd6d4d701d2816d1ba364a98b3531a6d8c0e
Asmaa Email <asmaaesmail0@gmail.com>
2020-09-06 11:53:05 +0200

    front

--- resources/views/site/favourites.blade.php
+++ resources/views/site/favourites.blade.php
@@ -48,13 +48,79 @@
          <div class="filter-buttons">
             <div class="list-view-button"><i class="fa fa-bars" aria-hidden="true"></i>  </div>
             <div class="grid-view-button"><i class="fa fa-th-large" aria-hidden="true"></i>  </div>
+            <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Dropdown button
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Action</a>
+    <a class="dropdown-item" href="#">Another action</a>
+    <a class="dropdown-item" href="#">Something else here</a>
+  </div>
+</div>
          </div>
       </div>
-      <div class="row">
-         <div class="col-md-6 parentfav">
+      <div class="row list list-view-filter">
+         <div class="col-md-6  col-sm-6 col-xs-12 parentfav listgrid">
             <div class="fleximgfav">
                <img src="{{asset('website/dist/img/asset6.png')}}" alt="">
-               <div>
+               <div class="descrpp">
+                  <p class="nameprodfav">زيت الارغان الاصلي</p>
+                  <b><span>100</span>  ريال سعودي </b>
+                  <b class="favpp">هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة  هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة</b>
+                  <div class="flex flexadd">
+                    <div class="deleteaa"><i class="far fa-trash-alt"></i></div>
+                    <div class="addcard">اضافة للسلة  <i class="fas fa-shopping-bag"></i></div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-6  col-sm-6 col-xs-12 parentfav listgrid">
+            <div class="fleximgfav">
+               <img src="{{asset('website/dist/img/asset6.png')}}" alt="">
+               <div class="descrpp">
+                  <p class="nameprodfav">زيت الارغان الاصلي</p>
+                  <b><span>100</span>  ريال سعودي </b>
+                  <b class="favpp">هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة  هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة</b>
+                  <div class="flex flexadd">
+                    <div class="deleteaa"><i class="far fa-trash-alt"></i></div>
+                    <div class="addcard">اضافة للسلة  <i class="fas fa-shopping-bag"></i></div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-6  col-sm-6 col-xs-12 parentfav listgrid">
+            <div class="fleximgfav">
+               <img src="{{asset('website/dist/img/asset6.png')}}" alt="">
+               <div class="descrpp">
+                  <p class="nameprodfav">زيت الارغان الاصلي</p>
+                  <b><span>100</span>  ريال سعودي </b>
+                  <b class="favpp">هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة  هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة</b>
+                  <div class="flex flexadd">
+                    <div class="deleteaa"><i class="far fa-trash-alt"></i></div>
+                    <div class="addcard">اضافة للسلة  <i class="fas fa-shopping-bag"></i></div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-6  col-sm-6 col-xs-12 parentfav listgrid">
+            <div class="fleximgfav">
+               <img src="{{asset('website/dist/img/asset6.png')}}" alt="">
+               <div class="descrpp">
+                  <p class="nameprodfav">زيت الارغان الاصلي</p>
+                  <b><span>100</span>  ريال سعودي </b>
+                  <b class="favpp">هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة  هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة</b>
+                  <div class="flex flexadd">
+                    <div class="deleteaa"><i class="far fa-trash-alt"></i></div>
+                    <div class="addcard">اضافة للسلة  <i class="fas fa-shopping-bag"></i></div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-6  col-sm-6 col-xs-12 parentfav listgrid">
+            <div class="fleximgfav">
+               <img src="{{asset('website/dist/img/asset6.png')}}" alt="">
+               <div class="descrpp">
                   <p class="nameprodfav">زيت الارغان الاصلي</p>
                   <b><span>100</span>  ريال سعودي </b>
                   <b class="favpp">هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة  هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة</b>
@@ -77,5 +143,35 @@
             $(this).parents(".parentfav").fadeOut(300);
             $(this).parents(".parentfav").remove(3);
         });
+</script>
+<script>
+const listViewButton = document.querySelector('.list-view-button');
+       const gridViewButton = document.querySelector('.grid-view-button');
+       const list = document.querySelector('.list.list-view-filter');
+
+       listViewButton.onclick = function () {
+       list.classList.remove('grid-view-filter');
+       list.classList.add('list-view-filter');
+       $(".listgrid").addClass( "col-md-6 listgrida" );
+       $(".listgrid").removeClass( "col-md-3 " );
+       $(".list-view-button").addClass( "classListGrid" );
+       $(".grid-view-button").removeClass( "classListGrid" );
+
+
+
+
+       }
+
+       gridViewButton.onclick = function () {
+       list.classList.remove('list-view-filter');
+       list.classList.add('grid-view-filter');
+       $(".listgrid").addClass( "col-md-3" );
+       $(".listgrid").removeClass( "col-md-6 listgrida" );
+       $(".grid-view-button").addClass( "classListGrid" );
+       $(".list-view-button").removeClass( "classListGrid" );
+
+
+       }
+
 </script>
 @endsection
