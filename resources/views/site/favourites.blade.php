2db31bc10f3d4cd29ef2e8e5a5b8c8e05c380bef
Asmaa Email <asmaaesmail0@gmail.com>
2020-09-06 14:16:48 +0200

    front

--- resources/views/site/favourites.blade.php
+++ resources/views/site/favourites.blade.php
@@ -35,36 +35,35 @@
 <section class="all-sections seccateg ">
    <div class="container">
       <div class="flexpagin">
-        <div class="pagenum">
+         <div class="pagenum">
             <span>عدد الصفحات :</span>
             <ul class="pagination">
-            <li><a href="#">1</a></li>
-            <li><a href="#">2</a></li>
-            <li><a href="#">3</a></li>
-            <li><a href="#">4</a></li>
-            <li><a href="#">5</a></li>
-         </ul>
-        </div>
+               <li><a href="#">1</a></li>
+               <li><a href="#">2</a></li>
+               <li><a href="#">3</a></li>
+               <li><a href="#">4</a></li>
+               <li><a href="#">5</a></li>
+            </ul>
+         </div>
          <div class="filter-buttons">
-    <div>
-    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-  <i class="fas fa-sort-amount-down"></i>
-  </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="#">Action</a>
-    <a class="dropdown-item" href="#">Another action</a>
-    <a class="dropdown-item" href="#">Something else here</a>
-  </div>
-    </div>
+            <div class="relative">
+               <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+               <i class="fas fa-sort-amount-down"></i>
+               </button>
+               <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <ul>
+                      <li>الخصم العالم</li>
+                  </ul>
+               </div>
+            </div>
             <div class="list-view-button"><i class="fa fa-bars" aria-hidden="true"></i>  </div>
             <div class="grid-view-button"><i class="fa fa-th-large" aria-hidden="true"></i>  </div>
             <div class="dropdown">
-
-</div>
+            </div>
          </div>
       </div>
       <div class="row list list-view-filter">
-         <div class="col-md-6  col-sm-6 col-xs-12 parentfav listgrid">
+         <div class="col-md-6  col-sm-4 col-xs-6 parentfav listgrid">
             <div class="fleximgfav">
                <img src="{{asset('website/dist/img/asset6.png')}}" alt="">
                <div class="descrpp">
@@ -72,13 +71,13 @@
                   <b><span>100</span>  ريال سعودي </b>
                   <b class="favpp">هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة  هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة</b>
                   <div class="flex flexadd">
-                    <div class="deleteaa"><i class="far fa-trash-alt"></i></div>
-                    <div class="addcard">اضافة للسلة  <i class="fas fa-shopping-bag"></i></div>
+                     <div class="deleteaa"><i class="far fa-trash-alt"></i></div>
+                     <div class="addcard">اضافة للسلة <i class="fas fa-cart-plus"></i></div>
                   </div>
                </div>
             </div>
          </div>
-         <div class="col-md-6  col-sm-6 col-xs-12 parentfav listgrid">
+         <div class="col-md-6  col-sm-4 col-xs-6 parentfav listgrid">
             <div class="fleximgfav">
                <img src="{{asset('website/dist/img/asset6.png')}}" alt="">
                <div class="descrpp">
@@ -86,50 +85,8 @@
                   <b><span>100</span>  ريال سعودي </b>
                   <b class="favpp">هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة  هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة</b>
                   <div class="flex flexadd">
-                    <div class="deleteaa"><i class="far fa-trash-alt"></i></div>
-                    <div class="addcard">اضافة للسلة  <i class="fas fa-shopping-bag"></i></div>
-                  </div>
-               </div>
-            </div>
-         </div>
-         <div class="col-md-6  col-sm-6 col-xs-12 parentfav listgrid">
-            <div class="fleximgfav">
-               <img src="{{asset('website/dist/img/asset6.png')}}" alt="">
-               <div class="descrpp">
-                  <p class="nameprodfav">زيت الارغان الاصلي</p>
-                  <b><span>100</span>  ريال سعودي </b>
-                  <b class="favpp">هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة  هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة</b>
-                  <div class="flex flexadd">
-                    <div class="deleteaa"><i class="far fa-trash-alt"></i></div>
-                    <div class="addcard">اضافة للسلة  <i class="fas fa-shopping-bag"></i></div>
-                  </div>
-               </div>
-            </div>
-         </div>
-         <div class="col-md-6  col-sm-6 col-xs-12 parentfav listgrid">
-            <div class="fleximgfav">
-               <img src="{{asset('website/dist/img/asset6.png')}}" alt="">
-               <div class="descrpp">
-                  <p class="nameprodfav">زيت الارغان الاصلي</p>
-                  <b><span>100</span>  ريال سعودي </b>
-                  <b class="favpp">هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة  هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة</b>
-                  <div class="flex flexadd">
-                    <div class="deleteaa"><i class="far fa-trash-alt"></i></div>
-                    <div class="addcard">اضافة للسلة  <i class="fas fa-shopping-bag"></i></div>
-                  </div>
-               </div>
-            </div>
-         </div>
-         <div class="col-md-6  col-sm-6 col-xs-12 parentfav listgrid">
-            <div class="fleximgfav">
-               <img src="{{asset('website/dist/img/asset6.png')}}" alt="">
-               <div class="descrpp">
-                  <p class="nameprodfav">زيت الارغان الاصلي</p>
-                  <b><span>100</span>  ريال سعودي </b>
-                  <b class="favpp">هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة  هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة</b>
-                  <div class="flex flexadd">
-                    <div class="deleteaa"><i class="far fa-trash-alt"></i></div>
-                    <div class="addcard">اضافة للسلة  <i class="fas fa-shopping-bag"></i></div>
+                     <div class="deleteaa"><i class="far fa-trash-alt"></i></div>
+                     <div class="addcard">اضافة للسلة <i class="fas fa-cart-plus"></i></div>
                   </div>
                </div>
             </div>
@@ -142,39 +99,39 @@
 @section('scripts')
 <script src="{{asset('website/dist/js/jquery.fancybox.min.js')}}"></script>
 <script>
-    $(".deleteaa").click(function() {
-            $(this).parents(".parentfav").fadeOut(300);
-            $(this).parents(".parentfav").remove(3);
-        });
+   $(".deleteaa").click(function() {
+           $(this).parents(".parentfav").fadeOut(300);
+           $(this).parents(".parentfav").remove(3);
+       });
 </script>
 <script>
-const listViewButton = document.querySelector('.list-view-button');
-       const gridViewButton = document.querySelector('.grid-view-button');
-       const list = document.querySelector('.list.list-view-filter');
+   const listViewButton = document.querySelector('.list-view-button');
+          const gridViewButton = document.querySelector('.grid-view-button');
+          const list = document.querySelector('.list.list-view-filter');
 
-       listViewButton.onclick = function () {
-       list.classList.remove('grid-view-filter');
-       list.classList.add('list-view-filter');
-       $(".listgrid").addClass( "col-md-6 listgrida" );
-       $(".listgrid").removeClass( "col-md-3 " );
-       $(".list-view-button").addClass( "classListGrid" );
-       $(".grid-view-button").removeClass( "classListGrid" );
-
-
+          listViewButton.onclick = function () {
+          list.classList.remove('grid-view-filter');
+          list.classList.add('list-view-filter');
+          $(".listgrid").addClass( "col-md-6 listgrida" );
+          $(".listgrid").removeClass( "col-md-3 " );
+          $(".list-view-button").addClass( "classListGrid" );
+          $(".grid-view-button").removeClass( "classListGrid" );
 
 
-       }
-
-       gridViewButton.onclick = function () {
-       list.classList.remove('list-view-filter');
-       list.classList.add('grid-view-filter');
-       $(".listgrid").addClass( "col-md-3" );
-       $(".listgrid").removeClass( "col-md-6 listgrida" );
-       $(".grid-view-button").addClass( "classListGrid" );
-       $(".list-view-button").removeClass( "classListGrid" );
 
 
-       }
+          }
+
+          gridViewButton.onclick = function () {
+          list.classList.remove('list-view-filter');
+          list.classList.add('grid-view-filter');
+          $(".listgrid").addClass( "col-md-3" );
+          $(".listgrid").removeClass( "col-md-6 listgrida" );
+          $(".grid-view-button").addClass( "classListGrid" );
+          $(".list-view-button").removeClass( "classListGrid" );
+
+
+          }
 
 </script>
 @endsection
