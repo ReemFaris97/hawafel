324a19212842c31e97bc6b8a42c8620bd680ef50
Asmaa Email <asmaaesmail0@gmail.com>
2020-09-10 14:59:15 +0200

    singleProduct

--- resources/views/site/single-product.blade.php
+++ resources/views/site/single-product.blade.php
@@ -10,7 +10,8 @@
 <!-- /////// |||||||||||||||||||| section |||||||||||||||||||| ///////// -->
 <section class="all-sections  about">
    <div class="container  ">
-   <p class="company-bransh-main"> </p>
+
+   <p class="singleprodlink"><a href="../">الرئيسية</a> / <a href="{{url('categories')}}">الاقسام</a>  / <a href="{{url('multimedia')}}">العناية بالبشرة</a>  / <a href="{{url('multimedia')}}">منتجات</a>  / <a href="{{url('single-product')}}">تفاصيل المنتج</a>  </p>
    <div class="padingback bacWhite ">
       <div class="row">
          <div class="col-xs-12 col-md-6 ">
@@ -65,49 +66,207 @@
             </div>
             <p class="disccolor"><span>3999 ريال</span> <span>4250 ريال</span> </p>
             <p class="p-detalies">هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحةهذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحةهذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة</p>
-            <p class="mountP"><span>الوحدات المتاحة</span></p>
-
-            <div class="custom_radio bordetopbotm">
-                <ul class="inlineradio">
-                    <li>
+            <div class="bordetopbotm">
+               <p class="mountP"><span>الوحدات المتاحة</span></p>
+               <div class="custom_radio ">
+                  <ul class="inlineradio">
+                     <li>
                         <input type="radio" id="featured-1" name="featured" checked=""><label for="featured-1">اسم الوحدة </label>
-                    </li>
-                    <li>
+                     </li>
+                     <li>
                         <input type="radio" id="featured-2" name="featured"><label for="featured-2"> اسم الوحدة  </label>
-                    </li>
-                    <li>
+                     </li>
+                     <li>
                         <input type="radio" id="featured-3" name="featured"><label for="featured-3"> اسم الوحدة  </label>
-                    </li>
-                </ul>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+            <div class="contentupdown">
+               <div class="number-input">
+                  <button onclick="this.parentNode.querySelector('.quantity').stepUp()" class="plus"> <i class="fas fa-plus"></i> </button>
+                  <input class="quantity" min="0" name="quantity" value="0" type="number">
+                  <button onclick="this.parentNode.querySelector('.quantity').stepDown()" class="minus"> <i class="fas fa-minus"></i> </button>
+               </div>
+               <div class="flex flexadd">
+                  <div class="likerr"><i class="far fa-heart"></i></div>
+                  <a href="{{url('cart')}}" class="addcard">اضافة للسلة <i class="fas fa-cart-plus"></i></a>
+               </div>
             </div>
          </div>
       </div>
    </div>
 </section>
-
-
-
 <!-- //////////////////////////////////////////////// -->
-
-<section class="all-sections">
+<section class="all-sections absbottom">
    <div class="container">
       <ul class="nav nav-tabs navmulti">
          <li class="active"><a data-toggle="tab" href="#home">تفاصيل المنتج </a></li>
          <li><a data-toggle="tab" href="#menu1">التقييمات</a></li>
-
       </ul>
       <div class="tab-content">
          <div id="home" class="tab-pane fade in active">
-
+            <p class="product-layName ptitle"> التفاصيل</p>
+            <p class="ptabscont">هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة
+               هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحةهذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة
+               هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحةهذا
+            </p>
          </div>
          <div id="menu1" class="tab-pane fade">
-
+            <div>
+               <div class="tabcont">
+                  <div class="row contentcommen">
+                     <div class="col-sm-7 col-xs-12">
+                        <p class="titlmulti">التقييمات</p>
+                        <div class="flexcommentstr">
+                            <div class="comments">
+                            <div class="comment-img">
+                                <img  src="{{asset('website/dist/img/man1.jpg')}}">
+                                <div class="comment-name">
+                                    <p >محمد ابراهيم عبدالله </p>
+                                    <div class="date">29/ 1/ 2019</div>
+                                </div>
+                            </div>
+                            <p class="comments-p">
+                                هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة هذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحةهذا النص هو مثال لنص يمكن ان يستبدل في هذة المساحة
+                            </p>
+                            </div>
+                            <div class="contentstr">
+                                <b>جيد</b>
+                                <div>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                </div>
+                            </div>
+                        </div>
+                     </div>
+                     <div class="col-sm-5 col-xs-12 ">
+                        <p class="titlmulti addcomrnt">اضافة تعليق</p>
+                        <form class="formsinglemulti">
+                           <div class="">
+                              <div class="form-group">
+                                 <textarea rows="3" type="text" class="form-control inputspn" placeholder="الرسالة"></textarea>
+                              </div>
+                           </div>
+                           <div class="rating">
+                              <input type="radio" name="star" id="star1" checked>
+                              <label for="star1">
+                              <img src="{{asset('website/dist/img/love.svg')}}" alt="Loved it" >
+                              <span>Loved it</span>
+                              </label>
+                              <input type="radio" name="star" id="star2">
+                              <label for="star2">
+                              <img src="{{asset('website/dist/img/liked.svg')}}" alt="Liked it">
+                              <span>Liked it</span>
+                              </label>
+                              <input type="radio" name="star" id="star3">
+                              <label for="star3">
+                              <img src="{{asset('website/dist/img/ok.svg')}}" alt="It's OK" >
+                              <span>It's OK</span>
+                              </label>
+                              <input type="radio" name="star" id="star4">
+                              <label for="star4">
+                              <img src="{{asset('website/dist/img/dislike.svg')}}" alt="Disliked it">
+                              <span>Disliked it</span>
+                              </label>
+                              <input type="radio" name="star" id="star5">
+                              <label for="star5">
+                              <img src="{{asset('website/dist/img/hated.svg')}}" alt="Hated it">
+                              <span>Hated it</span>
+                              </label>
+                           </div>
+                           <div class="aligne-center divwidthbtn">
+                              <button class="btn-hvr btn-flex">
+                              <span class="z-span">تقييم</span>
+                              </button>
+                           </div>
+                        </form>
+                     </div>
+                  </div>
+               </div>
+            </div>
          </div>
-
       </div>
    </div>
 </section>
 <!-- /////////////////////|||||||||||End sections |||||||||||||||||||| -->
+<!-- ///////////|||||||||||||||||||||||||||||||||||||/////////// -->
+<section class="recents all-sections">
+   <div class="container">
+      <p class="product-layName ptitle"> منجات ذات صلة</p>
+      <div class="row">
+         <div class="col-md-3 col-sm-4 col-xs-6">
+            <div class="fleximgfav">
+               <img src="{{asset('website/dist/img/asset6.png')}}" alt="">
+               <div class="descrpp">
+                  <a href="{{url('single-product')}}" class="nameprodfav">زيت الارغان الاصلي</a>
+                  <b><span>100</span> ريال سعودي </b>
+                  <div class="flex flexadd">
+                     <div class="likerr"><i class="far fa-heart"></i></div>
+                     <div class="addcard">اضافة للسلة <i class="fas fa-cart-plus"></i></div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-3 col-sm-4 col-xs-6">
+            <div class="fleximgfav">
+               <img src="{{asset('website/dist/img/asset6.png')}}" alt="">
+               <div class="descrpp">
+                  <a href="{{url('single-product')}}" class="nameprodfav">زيت الارغان الاصلي</a>
+                  <b><span>100</span> ريال سعودي </b>
+                  <div class="flex flexadd">
+                     <div class="likerr"><i class="far fa-heart"></i></div>
+                     <div class="addcard">اضافة للسلة <i class="fas fa-cart-plus"></i></div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-3 col-sm-4 col-xs-6">
+            <div class="fleximgfav">
+               <img src="{{asset('website/dist/img/asset6.png')}}" alt="">
+               <div class="descrpp">
+                  <a href="{{url('single-product')}}" class="nameprodfav">زيت الارغان الاصلي</a>
+                  <b><span>100</span> ريال سعودي </b>
+                  <div class="flex flexadd">
+                     <div class="likerr"><i class="far fa-heart"></i></div>
+                     <div class="addcard">اضافة للسلة <i class="fas fa-cart-plus"></i></div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-3 col-sm-4 col-xs-6">
+            <div class="fleximgfav">
+               <img src="{{asset('website/dist/img/asset6.png')}}" alt="">
+               <div class="descrpp">
+                  <a href="{{url('single-product')}}" class="nameprodfav">زيت الارغان الاصلي</a>
+                  <b><span>100</span> ريال سعودي </b>
+                  <div class="flex flexadd">
+                     <div class="likerr"><i class="far fa-heart"></i></div>
+                     <div class="addcard">اضافة للسلة <i class="fas fa-cart-plus"></i></div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-3 col-sm-4 col-xs-6">
+            <div class="fleximgfav">
+               <img src="{{asset('website/dist/img/asset6.png')}}" alt="">
+               <div class="descrpp">
+                  <a href="{{url('single-product')}}" class="nameprodfav">زيت الارغان الاصلي</a>
+                  <b><span>100</span> ريال سعودي </b>
+                  <div class="flex flexadd">
+                     <div class="likerr"><i class="far fa-heart"></i></div>
+                     <div class="addcard">اضافة للسلة <i class="fas fa-cart-plus"></i></div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!-- ///////|||||||||||||||||||||||||||||||||||||||||/////////// -->
 @endsection
 @section('scripts')
 <script src="{{asset('website/dist/js/jquery.fancybox.min.js')}}"></script>
@@ -137,6 +296,10 @@
        }
    });
    /*End Product*/
-
+</script>
+<script>
+   $(".likerr").click(function() {
+       $(this).toggleClass("i_liked");
+   });
 </script>
 @endsection
